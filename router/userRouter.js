4292baa27b40235ad7ab3eacde56bf3c3089191c
dabasishJoy <dabasish@6sensehq.com>
2023-04-06 23:05:01 +0600

    user validation

--- router/userRouter.js
+++ router/userRouter.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 // internal imports
 const UserController = require("../controller/userController");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/userValidator");
 
 // creating a router
 const router = express.Router();
@@ -10,7 +14,12 @@ const router = express.Router();
 router.get("/", UserController.getUsers);
 
 // create user
-router.post("/", UserController.CreateUser);
+router.post(
+  "/",
+  addUserValidators,
+  addUserValidationHandler,
+  UserController.CreateUser
+);
 
 // update user
 router.get("/:userId", UserController.getUser);
